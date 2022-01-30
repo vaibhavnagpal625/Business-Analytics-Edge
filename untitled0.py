53042a74a67b00bfa923bfc689c25801029ab877
vaibhavnagpal625 <98686665+vaibhavnagpal625@users.noreply.github.com>
2022-01-30 15:39:21 +0530

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 30 15:19:12 2022
+
+@author: DELL
+"""
+
+Thapar_batch = [1,2,3,4,5,6,7,8]
+print(Thapar_batch)
+import matplotlib.pyplot as pt
+pt.plot(Thapar_batch)
